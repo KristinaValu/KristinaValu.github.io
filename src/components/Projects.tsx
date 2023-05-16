468f5235ea36720cd0cdb28a58ae73e1b572883a
Valeras Narbutas <valerasn@gmail.com>
2023-05-16 16:13:22 +0300

    pataisiau

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,6 +2,7 @@ function Projects(){
     return(
         <>
         <div>
+            {/* Project asasas */}
             <h1>Projects  -  Coming soon..... :):) </h1>
         </div>
         </>
